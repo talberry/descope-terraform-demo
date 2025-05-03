8b3de47d731ca992981b096d86f7303d69a311a4
Kevin J Gao <32936811+gaokevin1@users.noreply.github.com>
2025-05-03 11:09:13 -0700

    added new workflows

--- main.tf
+++ main.tf
@@ -4,14 +4,44 @@ provider "descope" {
 }
 
 resource "descope_project" "my_project" {
-  name = "CI/CD Demo Project"
+  name = "BCBS Stellarus CIAM Demo - PROD"
+  environment = "production"
 
   flows = {
     "sign-up-or-in" = {
       data = file("${path.module}/flows/sign-up-or-in.json")
     },
+    "sign-in" = {
+      data = file("${path.module}/flows/sign-in.json")
+    },
+    "sign-up" = {
+      data = file("${path.module}/flows/sign-up.json")
+    },
     "forgot-password" = {
       data = file("${path.module}/flows/forgot-password.json")
     }
+  }
+
+  connectors = {
+    "http": [
+      {
+        name = "Radiant Logic"
+        description = "Radiant Logic Connector for JIT"
+        base_url = "https://test-descope.free.beeceptor.com"
+      }
+    ],
+    "hibp": [
+      {
+        name = "Have I Been Pwned"
+        description = "Connector for Checking Password Breaches"
+      }
+    ],
+    "forter": [
+      {
+        name = "Forter"
+        secret_key = "<secret_key>"
+        site_id = "<example_site_id>"
+      }
+    ]
   }
 }
