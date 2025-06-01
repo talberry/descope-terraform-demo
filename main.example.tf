006fc3fc2a2e26bca7b1f337ee30a78569d1b3e2
Kevin J Gao <32936811+gaokevin1@users.noreply.github.com>
2025-06-01 15:05:32 -0700

    added main

--- main.example.tf
+++ main.example.tf
@@ -35,6 +35,29 @@ resource "descope_project" "my_project" {
         name = "Have I Been Pwned"
         description = "Connector for Checking Password Breaches"
       }
+    ],
+    "forter": [
+      {
+        name = "Forter"
+        secret_key = "<secret_key>"
+        site_id = "<example_site_id>"
+      }
+    ],
+    "smtp": [
+      {
+        name = "SMTP Connector"
+        authentication = {
+          "username" = "a1590ce4b6f834"
+          "password" = "bb6e89779e35ff"
+        }
+        sender = {
+          "email" = "bd3ed16c48-30b04d+1@inbox.mailtrap.io"
+        }
+        server = {
+          "host" = "sandbox.smtp.mailtrap.io"
+          "port" = "587"
+        }
+      }
     ]
   }
 }
