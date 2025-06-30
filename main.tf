9f766783079ec00a4583e0e42fe8efdf4d28e9b4
Kevin J Gao <32936811+gaokevin1@users.noreply.github.com>
2025-06-30 11:13:10 -0700

    added new main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,9 @@ provider "descope" {
   project_id     = var.project_id
   management_key = var.management_key
 }
-
-resource "descope_project" "project" {
+resource "descope_project" "my_project" {
   name = "Terraform Demo - PROD"
   environment = "production"
-
   flows = {
     "sign-up-or-in" = {
       data = file("${path.module}/flows/sign-up-or-in.json")
@@ -18,27 +16,40 @@ resource "descope_project" "project" {
       data = file("${path.module}/flows/sign-up.json")
     }
   }
-
   connectors = {
+    "http": [
+      {
+        name = "Radiant Logic"
+        description = "Radiant Logic Connector for JIT"
+        base_url = "https://test-descope.free.beeceptor.com"
+      }
+    ],
     "hibp": [
       {
         name = "Have I Been Pwned"
         description = "Connector for Checking Password Breaches"
       }
     ],
+    "forter": [
+      {
+        name = "Forter"
+        secret_key = "<secret_key>"
+        site_id = "<example_site_id>"
+      }
+    ],
     "smtp": [
       {
-        name = "Email Connector"
-        description = "Email Connector"
+        name = "SMTP Connector"
+        authentication = {
+          "username" = "a1590ce4b6f834"
+          "password" = "bb6e89779e35ff"
+        }
         sender = {
-          email = "support@company.com"
+          "email" = "bd3ed16c48-30b04d+1@inbox.mailtrap.io"
         }
         server = {
-          host = "587"
-        }
-        authentication = {
-          username = "test"
-          password = "password1"
+          "host" = "sandbox.smtp.mailtrap.io"
+          "port" = "587"
         }
       }
     ]
